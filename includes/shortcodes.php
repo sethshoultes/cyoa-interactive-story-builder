4f9bdb8d0fc94b31913cafe76bf9828a9cca6177
Seth <seth@smartwebutah.com>
2024-11-07 11:26:17 -0700

    Move code placement

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -476,6 +476,97 @@ function iasb_display_strength_shortcode() {
 }
 add_shortcode('display_strength', 'iasb_display_strength_shortcode');
 
+function iasb_render_conditional_content_block($attributes, $content) {
+    // error_log('iasb_render_conditional_content_block called with: ' . print_r($attributes, true));
+     $shortcode_str = '[conditional_content';
+     if (isset($attributes['id'])) {
+         $shortcode_str .= ' id="' . esc_attr($attributes['id']) . '"';
+     }
+     if (isset($attributes['condition'])) {
+         $shortcode_str .= ' condition="' . esc_attr($attributes['condition']) . '"';
+     }
+     $shortcode_str .= ']' . ($attributes['content'] ?? '') . '[/conditional_content]';
+     //error_log('Generated shortcode: ' . $shortcode_str);
+     return do_shortcode($shortcode_str);
+ }
+ function iasb_render_inventory_block() {
+     $user_id = get_current_user_id();
+     $story_id = get_the_ID();
+     $state_manager = new IASB_State_Manager($user_id, $story_id, 'default_character');
+     $inventory = $state_manager->get_inventory();
+ 
+     $output = '<ul class="player-inventory">';
+     if (empty($inventory)) {
+         $output .= '<li>' . esc_html__('Your inventory is empty.', 'story-builder') . '</li>';
+     } else {
+         foreach ($inventory as $item_name => $quantity) {
+             $output .= '<li>' . esc_html($item_name) . ': ' . esc_html($quantity) . '</li>';
+         }
+     }
+     $output .= '</ul>';
+     
+     return $output;
+ }
+ add_shortcode('display_inventory', 'iasb_render_inventory_block');
+ 
+ /**
+  * Adds the given item to the current user's inventory, with the given quantity.
+  * 
+  * @param int $quantity The number of items to add. Defaults to 1.
+  * 
+  * @return string Message indicating the result of the operation.
+  */
+ function iasb_add_to_inventory($atts) {
+     $atts = shortcode_atts(array(
+         'item' => '',
+         'quantity' => 1,
+     ), $atts);
+ 
+     if (empty($atts['item'])) {
+         return __('Error: No item specified.', 'story-builder');
+     }
+ 
+     $user_id = get_current_user_id();
+     $story_id = get_the_ID();
+     $transient_name = 'iasb_inventory_added_' . $user_id . '_' . $story_id . '_' . sanitize_title($atts['item']);
+ 
+     // Check if the shortcode has already been executed for this item
+     if (get_transient($transient_name)) {
+         return ''; // Return empty if already executed
+     }
+ 
+     $character_id = 'default_character';
+     $state_manager = new IASB_State_Manager($user_id, $story_id, $character_id);
+     
+     // Add item to inventory
+     $state_manager->add_to_global_inventory($atts['item'], $atts['quantity']);
+ 
+     // Set the transient to indicate the shortcode has been executed for this item
+     set_transient($transient_name, true, 30 * MINUTE_IN_SECONDS); // Expires after 30 minutes
+ 
+     // Return a message
+     return sprintf(__('Added %d %s to your inventory.', 'story-builder'), $atts['quantity'], $atts['item']);
+ }
+ add_shortcode('add_to_inventory', 'iasb_add_to_inventory');
+ 
+ function iasb_render_add_to_inventory_block($attributes) {
+     $item = $attributes['item'] ?? '';
+     $quantity = $attributes['quantity'] ?? 1;
+     
+     if (empty($item)) {
+         return 'Error: No item specified.';
+     }
+ 
+     $user_id = get_current_user_id();
+     $story_id = get_the_ID();
+     $character_id = 'default_character'; // Replace with the appropriate character ID
+     $state_manager = new IASB_State_Manager($user_id, $story_id, $character_id);
+     
+     $state_manager->update_inventory($item, $quantity, 'add');
+     
+     return 'Added ' . $quantity . ' ' . $item . '(s) to your inventory.';
+ }
+ 
 
 /* Gutenberg Blocks */
 // Register Gutenberg blocks
@@ -584,96 +675,3 @@ function iasb_register_conditional_content_block() {
     ));
 }
 add_action('init', 'iasb_register_conditional_content_block');
-
-function iasb_render_conditional_content_block($attributes, $content) {
-   // error_log('iasb_render_conditional_content_block called with: ' . print_r($attributes, true));
-    $shortcode_str = '[conditional_content';
-    if (isset($attributes['id'])) {
-        $shortcode_str .= ' id="' . esc_attr($attributes['id']) . '"';
-    }
-    if (isset($attributes['condition'])) {
-        $shortcode_str .= ' condition="' . esc_attr($attributes['condition']) . '"';
-    }
-    $shortcode_str .= ']' . ($attributes['content'] ?? '') . '[/conditional_content]';
-    //error_log('Generated shortcode: ' . $shortcode_str);
-    return do_shortcode($shortcode_str);
-}
-function iasb_render_inventory_block() {
-    $user_id = get_current_user_id();
-    $story_id = get_the_ID();
-    $state_manager = new IASB_State_Manager($user_id, $story_id, 'default_character');
-    $inventory = $state_manager->get_inventory();
-
-    $output = '<ul class="player-inventory">';
-    if (empty($inventory)) {
-        $output .= '<li>' . esc_html__('Your inventory is empty.', 'story-builder') . '</li>';
-    } else {
-        foreach ($inventory as $item_name => $quantity) {
-            $output .= '<li>' . esc_html($item_name) . ': ' . esc_html($quantity) . '</li>';
-        }
-    }
-    $output .= '</ul>';
-    
-    return $output;
-}
-add_shortcode('display_inventory', 'iasb_render_inventory_block');
-
-/**
- * Adds the given item to the current user's inventory, with the given quantity.
- * 
- * @param int $quantity The number of items to add. Defaults to 1.
- * 
- * @return string Message indicating the result of the operation.
- */
-function iasb_add_to_inventory($atts) {
-    $atts = shortcode_atts(array(
-        'item' => '',
-        'quantity' => 1,
-    ), $atts);
-
-    if (empty($atts['item'])) {
-        return __('Error: No item specified.', 'story-builder');
-    }
-
-    $user_id = get_current_user_id();
-    $story_id = get_the_ID();
-    $transient_name = 'iasb_inventory_added_' . $user_id . '_' . $story_id . '_' . sanitize_title($atts['item']);
-
-    // Check if the shortcode has already been executed for this item
-    if (get_transient($transient_name)) {
-        return ''; // Return empty if already executed
-    }
-
-    $character_id = 'default_character';
-    $state_manager = new IASB_State_Manager($user_id, $story_id, $character_id);
-    
-    // Add item to inventory
-    $state_manager->add_to_global_inventory($atts['item'], $atts['quantity']);
-
-    // Set the transient to indicate the shortcode has been executed for this item
-    set_transient($transient_name, true, 30 * MINUTE_IN_SECONDS); // Expires after 30 minutes
-
-    // Return a message
-    return sprintf(__('Added %d %s to your inventory.', 'story-builder'), $atts['quantity'], $atts['item']);
-}
-
-add_shortcode('add_to_inventory', 'iasb_add_to_inventory');
-add_shortcode('add_to_inventory', 'iasb_add_to_inventory');
-
-function iasb_render_add_to_inventory_block($attributes) {
-    $item = $attributes['item'] ?? '';
-    $quantity = $attributes['quantity'] ?? 1;
-    
-    if (empty($item)) {
-        return 'Error: No item specified.';
-    }
-
-    $user_id = get_current_user_id();
-    $story_id = get_the_ID();
-    $character_id = 'default_character'; // Replace with the appropriate character ID
-    $state_manager = new IASB_State_Manager($user_id, $story_id, $character_id);
-    
-    $state_manager->update_inventory($item, $quantity, 'add');
-    
-    return 'Added ' . $quantity . ' ' . $item . '(s) to your inventory.';
-}
